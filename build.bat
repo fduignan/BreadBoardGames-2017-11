28a28561e70dc4bff3ff04fa9738ad64e9a28b30
Frank Duignan <frank.duignan@gmail.com>
2017-11-16 20:41:55 +0000

    Fixed up batch file for use with Windows

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
-arm-none-eabi-gcc -static -mthumb -fno-rtti -fno-exceptions  -g -mcpu=cortex-m3 *.cpp -T linker_script.ld -o main.elf -nostartfiles -std=c++11 -Xlinker --cref -Xlinker -Map -Xlinker main.map
-arm-none-eabi-objcopy -g -O binary main.elf main.bin  
+@echo off
+@echo Go to http://ioprog.com/bbg for instructions
+..\arm_sdk\bin\arm-none-eabi-gcc -static -mthumb -fno-rtti -fno-exceptions  -g -mcpu=cortex-m3 *.cpp -T linker_script.ld -o main.elf -nostartfiles -std=c++11 -Xlinker --cref -Xlinker -Map -Xlinker main.map
+..\arm_sdk\bin\arm-none-eabi-objcopy -g -O ihex main.elf main.hex  
+..\arm_sdk\bin\arm-none-eabi-objcopy -g -O binary main.elf main.bin  
+echo Make sure the ST-Link device is plugged in and the CPU is wired to it.
+pause
+..\arm_sdk\flasher\bin\st-flash.exe write main.bin 0x8000000
+echo Hopefully that worked! :-)
+echo (you may have to press the reset button on the CPU board)
